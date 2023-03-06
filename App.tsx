d435b673a30b4f0dc94ae452dd47aef26c0c412a
Floriian <lovasifloriian@outlook.hu>
2023-03-06 09:27:50 +0100

    2 files modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,11 +1,56 @@
 import * as React from 'react';
-import './style.css';
 
 export default function App() {
+  const user = 'root';
+  const hostname = 'localhost';
+  const [commands, setCommands] = React.useState<Array<string>>([]);
+  const [userCommand, setUsercommand] = React.useState<string>();
+  const ref = React.useRef<HTMLInputElement>();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (userCommand != 'clear') {
+      setCommands([...commands, userCommand]);
+    }
+  };
+
+  React.useEffect(() => {
+    if (userCommand === 'clear') {
+      setCommands([]);
+    }
+  }, [userCommand]);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'black',
+        color: 'white',
+        fontFamily: 'consolas',
+      }}
+    >
+      <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+        <p>
+          {hostname}${user}:
+          <input
+            type="text"
+            style={{
+              backgroundColor: 'black',
+              border: 'none',
+              color: 'white',
+            }}
+            value={userCommand}
+            onChange={(e) => setUsercommand(e.target.value)}
+            ref={ref}
+          />
+        </p>
+      </form>
+      {commands.map((command, i) => (
+        <p key={i} style={{ color: 'white' }}>
+          {hostname}${user}: {command}
+        </p>
+      ))}
     </div>
   );
 }
